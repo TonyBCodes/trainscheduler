ab1cff0f2102f91a0b0bd538bf879f6ada46a6c4
TonyBCodes <34422871+TonyBCodes@users.noreply.github.com>
2018-03-22 13:09:36 -0400

    new changes

--- Script1.js
+++ Script1.js
@@ -1,33 +1,78 @@
-// JavaScript source code
+$(document).ready(function () {
 
-// Initialize Firebase
-// TODO: Replace with your project's customized code snippet
-var config = {
-    //apiKey: "<API_KEY>",
-    //authDomain: "test-7a585.firebaseapp.com",
-    databaseURL: "https://test-7a585.firebaseio.com/",
-    //storageBucket: "<BUCKET>.appspot.com",
-    //messagingSenderId: "<SENDER_ID>",
-};
-firebase.initializeApp(config);
+    //----- global variables-----------------------------------
 
-var info = {
-    "one": 1,
-    "two": 2,
-    "three": 3
-};
+    // Initialize Firebase
+    var config = {
+        databaseURL: "https://test-7a585.firebaseio.com/",
+    };
+    firebase.initializeApp(config);
+    var db = firebase.database();
+    var trainnum = db.ref("trainnumber");
 
-var trans;
-
-var db = firebase.database();
+    var traininfo = {
+        "trainnumber": 1,
+        "traindest": 2,
+        "trainstart": 3,
+        "trainfreq": 4
+    };
 
 
-firebase.database().ref().set({ info });
+    //----- functions------------------------------------------
 
-firebase.database().ref().push({ info });
+    function updateclock() {
+        $("#clock").html(moment().format('D MMMM YYYY H:mm:ss'));
+    }
 
-db.ref().on("child_changed", function (snapshot) {
-    alert("child changed");
-    console.log(snapshot.val());
-    trans = snapshot;
+    function gotdata(number) {
+        var trinform = (number.val());
+        var keys = Object.keys(trinform);
+        console.log(keys);
+        for (var i = 0; i < keys.length; i++) {
+            var k = keys[i];
+            var ti = trinform[k].traininfo.traindest; 
+            console.log(ti);
+        }
+
+
+    }
+
+
+
+
+
+
+    //----- end of functions-----------------------------------
+
+    //----- start----------------------------------------------
+    //$(window).on("load", function () {
+
+    firebase.database().ref().push({ traininfo });
+
+    firebase.database().ref().push({ traininfo });
+
+    db.ref().on("child_changed", function (snapshot) {
+        alert("child changed");
+        console.log(snapshot.val());
+        trans = snapshot;
+    });
+
+
+    setInterval(updateclock, 1000);
+
+    //db.ref().on("value", function (snapshot) {
+    //    console.log(snapshot.val());
+    //    $("#train").text(snapshot.val().traininfo.trainnumber);
+    //})
+
+    db.ref().on("value", function (snapshot) {
+        console.log(snapshot.val());
+        gotdata(snapshot);
+
+    });
+
+
+    //});
+
+
 });
